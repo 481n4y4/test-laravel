c5ae0b8a0931cb2fc87874ebb2c59eb89d8fd185
Abinaya <141475809+481n4y4@users.noreply.github.com>
2026-02-02 09:46:33 +0700

    📌 chore(api): correct the route

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,13 @@ use App\Http\Controllers\AuthController;
 Route::post('/login-jwt', [AuthController::class, 'loginJwt']);
 Route::get('/users', [AuthController::class, 'users']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::get('/barang', [BarangController::class, 'index']);
+Route::get('/barang/{id}', [BarangController::class, 'show']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('/barang', [BarangController::class, 'index']);
     Route::post('/barang', [BarangController::class, 'store']);
-    Route::get('/barang/{id}', [BarangController::class, 'show']);
     Route::put('/barang/{id}', [BarangController::class, 'update']);
     Route::delete('/barang/{id}', [BarangController::class, 'destroy']);
 });
